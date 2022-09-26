346ade5148ea110d4e034c7730455803aac3bc2d
Luke Brogee <lukebrogee@gmail.com>
2022-09-25 20:48:13 -0400

    Initial commit

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,24 @@
+public class PartyAffiliation {
+    public static void main(String[] args) {
+
+        System.out.println("What is your party affiliation");
+        String partyAffiliation = "Q";
+
+        if (partyAffiliation == "D"){
+            System.out.println("You are a donkey");
+        }else if (partyAffiliation == "R"){
+            System.out.println("You are an Elephant");
+        }else if (partyAffiliation == "I"){
+            System.out.println("You are a man");
+        }else{
+            System.out.println("You are an Other");
+        }
+
+
+
+
+
+
+
+    }
+}
